60b02fda7676d29b546bc578a7a1d75ebd100064
Chrisy846 <c.mcqueen@newcastle.ac.uk>
2017-06-11 16:31:44 +0100

    Admin now has CRUD on main static data screens

--- src/main/webapp/app/entities/prediction/prediction.controller.js
+++ src/main/webapp/app/entities/prediction/prediction.controller.js
@@ -5,13 +5,25 @@
         .module('predictimoApp')
         .controller('PredictionController', PredictionController);
 
-    PredictionController.$inject = ['Prediction'];
+    PredictionController.$inject = ['Prediction', 'Principal'];
 
-    function PredictionController(Prediction) {
+    function PredictionController(Prediction, Principal) {
 
         var vm = this;
+        vm.account = null;
+        vm.isAdmin = false;
 
         vm.predictions = [];
+        
+        Principal.identity().then(function (account) {
+            console.log(account);
+            vm.account = account;
+           
+            for (var i = 0; i < vm.account.authorities.length; i++) {
+                if (vm.account.authorities[i] === "ROLE_ADMIN")
+                    vm.isAdmin = true;
+            }
+        });
 
         loadAll();
 
